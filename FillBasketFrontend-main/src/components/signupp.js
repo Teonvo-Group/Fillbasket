63ddb5fbf9cc83c7467dc012e6c8a0f460fd0958
Sameer Saifi <sameer6396880994@gmail.com>
2024-05-18 11:45:08 +0530

    no cors

--- FillBasketFrontend-main/src/components/signupp.js
+++ FillBasketFrontend-main/src/components/signupp.js
@@ -18,6 +18,7 @@ export default class Signupp extends Component {
     console.log(fname, lname, email, password);
     fetch(`${process.env.REACT_APP_BACKEND_URL}/register`, {
       method: "POST",
+      mode: 'no-cors',
       crossDomain: true,
       headers: {
         "Content-Type": "application/json",
